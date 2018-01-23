a6739703dc3c22c0e8d8b7488f071207f967e64a
Rafael Nascimento de Sousa <rafaelsousa2187@gmail.com>
2018-01-23 00:30:41 -0200

    Add files via upload

---
+++ AutocompleteTrees-master/AutocompleteTrees/src/autocompletetrees/Main.java
@@ -0,0 +1,63 @@
+package autocompletetrees;
+
+import autocompletetrees.gui.Menu;
+import autocompletetrees.logic.Logic;
+import autocompletetrees.logic.tree.Trie;
+import autocompletetrees.logic.tree.TrieNode;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Menu m = new Menu();
+        Trie t = new Trie();
+        Logic l = new Logic();
+        TrieNode root = t.getRoot();
+        Scanner sc = new Scanner(System.in);
+        int op;
+        System.out.println("Autocomplete Trees =)");
+        //Do case 1 pra baixo, chamar da classe Logic
+        do {
+            op = m.menuEntrada(sc);
+            switch(op) {
+                case 0:
+                    m.exit();
+                    break;
+                case 1:
+                    l.addPalavra(t);
+                    break;
+                case 2:
+                    l.rmvPalavra(t);
+                    break;
+                case 3:
+                    l.searchPalavra(t);
+                    break;
+                case 4:
+                    l.printPalavras(root);
+                    break;
+                case 5:
+                    l.showRadicais(root);
+                    break;
+                case 6:
+                    l.listCount(root);
+                    break;
+                default:
+                    m.dispararErro();
+                    break;
+            }
+        } while(op != 0);
+//        System.out.println("Mostrando caminhos:");
+//        t.inserir("bola");
+//        t.inserir("bolinha");
+//        t.inserir("bolo");
+//        t.inserir("bolor");
+//        t.inserir("bolorado");
+//        t.inserir("barulho");
+//        t.inserir("belem");
+//        t.inserir("casa");
+//        t.inserir("deserto");
+//        String s = Character.toString(root.getContent());
+//        l.getTree(root, s);
+//        l.list(l.getList());
+//        l.list(l.matchPalavras("b", l.getList()));
+    }
+}
---
+++ AutocompleteTrees-master/AutocompleteTrees/src/autocompletetrees/logic/Logic.java
@@ -0,0 +1,98 @@
+package autocompletetrees.logic;
+
+import autocompletetrees.gui.Menu;
+import autocompletetrees.logic.tree.Trie;
+import autocompletetrees.logic.tree.TrieNode;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Logic {
+
+    Menu m = new Menu();
+    LinkedList<String> list = new LinkedList<>();
+    Scanner sc = new Scanner(System.in);
+    
+    public void getTree(TrieNode no, String name) {
+        for (TrieNode filho : no.getChildren()) {
+            System.out.println(name + filho.getContent());
+            getTree(filho, name + filho.getContent());
+        }
+    }
+
+    public LinkedList<String> getList() {
+        return list;
+    }
+
+    public void list(LinkedList<String> ls) {
+        for (String s : ls) {
+            System.out.println("-" + s);
+        }
+        System.out.println("");
+    }
+    
+    public void listCount(TrieNode t) {
+        for (String s : this.getList()) {
+            System.out.println("-("+t.getCountRepeat()+")" + s);
+        }
+        System.out.println("");
+    }
+
+    public LinkedList<String> matchPalavras(String s, LinkedList<String> l) {
+        LinkedList<String> matchList = new LinkedList<>();
+        if (s.length() >= 3) {
+            s += ".*";
+            for (String str : l) 
+                if (str.matches(s)) 
+                    matchList.add(str);
+        }
+        return matchList;
+    }
+
+    public void addPalavra(Trie t) {
+        m.inserirPalavraMenu();
+        String P = sc.next();
+        if(this.list.contains(P)==true){
+            t.IncrementoCountRepeat(P);
+        }else{    
+        t.inserir(P);
+        list.add(P);
+        }
+    }
+
+    public void rmvPalavra(Trie t) {
+        m.removerPalavraMenu();
+        String P = sc.next();
+        if (t.existe(P) == 0) {
+            System.err.println("\nA palavra " + P + " não está listada!");
+        } else if (list.isEmpty()) {
+            System.err.println("\nNão existem palavras no dicionário!");
+        } else {
+            t.remover(P);
+            this.list.remove(P);
+        }
+    }
+
+    public void printPalavras(TrieNode t) {
+        if (!list.isEmpty()) {
+            System.out.println("\nMostrando todas as palavras:");
+            this.list(this.getList());
+        } else 
+            System.err.println("\nNão existem palavras no dicionário!");
+    }
+
+    public void searchPalavra(Trie t) {
+        m.buscarPalavraMenu();
+        String P = sc.next();
+        if (matchPalavras(P, list).isEmpty()) 
+            System.err.printf("\nNão foram encontradas palavras com o radical \"%s\"\n!", P);
+        else if (list.isEmpty()) 
+            System.err.println("\nNão existem palavras no dicionário!");
+         else 
+            this.list(this.matchPalavras(P, list));
+    }
+    
+    public void showRadicais(TrieNode no){
+        System.out.println("\nMostrando todos os caminhos:");
+        getTree(no, Character.toString(no.getContent()));
+    }
+}
